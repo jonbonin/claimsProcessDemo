8767ca49f24c9237147eebbf7c48e612a863cacd
Jon Bonin <jon.e.bonin@gmail.com>
2019-08-07 15:51:14 -0500

    adding very basic functionality of site

---
+++ JS/claimsProcess.js
@@ -0,0 +1,16 @@
+(function () {
+    customElements.define('claims-process', class extends HTMLElement {
+        constructor() {
+            super();
+            const shadowRoot = this.attachShadow({ mode: 'open' });
+            const template = document.currentScript.ownerDocument.querySelector('#template');
+            const content = template.content;
+
+            shadowRoot.appendChild(content.cloneNode(true));
+
+            //make sure that sections are accuratly being displayed
+            this.shadowRoot.querySelector("#emplUpdateWrapper").style.display = 'none';
+            this.shadowRoot.querySelector("#dspEmplInfo").style.display = 'block';
+        };
+    });
+})()
